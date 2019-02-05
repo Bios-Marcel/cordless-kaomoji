a2e2931c8ca389fb9e860e86a6a6014a067587cc
Marcel Schramm <marceloschr@googlemail.com>
2019-02-05 16:00:17 +0100

    Use regex instead of fix and replace, so that it also workes when there are no characters around the kaomojis

--- kaomoji.js
+++ kaomoji.js
@@ -1,97 +1,86 @@
 function onMessageSend(message) {
     //Happy
-    message = message
-        .split(";uwu;").join("(´･ω･`)")
-        .split(";yay;").join("o(>ω<)o")
-        .split(";sparkle;").join("(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧")
-        .split(";smile;").join("(´･ᴗ･ ` )");
+    message = replaceKaomojiCode(message, ";uwu;", "(´･ω･`)")
+    message = replaceKaomojiCode(message, ";yay;", "o(>ω<)o")
+    message = replaceKaomojiCode(message, ";sparkle;", "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧")
+    message = replaceKaomojiCode(message, ";smile;", "(´･ᴗ･ ` )");
 
     //dissatisfied
-    message = message
-        .split(";fucku;").join("凸(￣ヘ￣)")
-        .split(";seriously;").join("(；⌣̀_⌣́)")
-        .split(";wtf;").join("(」°ロ°)」");
+    message = replaceKaomojiCode(message, ";fucku;", "凸(￣ヘ￣)")
+    message = replaceKaomojiCode(message, ";seriously;", "(；⌣̀_⌣́)")
+    message = replaceKaomojiCode(message, ";wtf;", "(」°ロ°)」");
 
     //anger
-    message = message
-        .split(";angry;").join("(・`ω´・)")
-        .split(";ikillyou;").join("↑_(ΦwΦ)Ψ")
-        .split(";rage;").join("٩(╬ʘ益ʘ╬)۶");
+    message = replaceKaomojiCode(message, ";angry;", "(・`ω´・)")
+    message = replaceKaomojiCode(message, ";ikillyou;", "↑_(ΦwΦ)Ψ")
+    message = replaceKaomojiCode(message, ";rage;", "٩(╬ʘ益ʘ╬)۶");
 
     //empathy
-    message = message
-        .split(";pat;").join("(ｏ・_・)ノ”(ノ_<、)");
+    message = replaceKaomojiCode(message, ";pat;", "(ｏ・_・)ノ”(ノ_<、)");
 
     //embarassment
-    message = message
-        .split(";sweat;").join("(⌒_⌒;)")
-        .split(";embarassed;").join("(⁄ ⁄•⁄ω⁄•⁄ ⁄)");
+    message = replaceKaomojiCode(message, ";sweat;", "(⌒_⌒;)")
+    message = replaceKaomojiCode(message, ";embarassed;", "(⁄ ⁄•⁄ω⁄•⁄ ⁄)");
 
     //love
-    message = message
-        .split(";love;").join("(❤ω❤)");
+    message = replaceKaomojiCode(message, ";love;", "(❤ω❤)");
 
     //sadness
-    message = message
-        .split(";doh;").join("(>_<)")
-        .split(";cry;").join(".｡･ﾟﾟ･(＞_＜)･ﾟﾟ･｡.");
+    message = replaceKaomojiCode(message, ";doh;", "(>_<)")
+    message = replaceKaomojiCode(message, ";cry;", ".｡･ﾟﾟ･(＞_＜)･ﾟﾟ･｡.");
 
     //fear
-    message = message
-        .split(";waaa;").join("＼(º □ º l|l)/")
-        .split(";ohfuck;").join("＼(〇_ｏ)／");
+    message = replaceKaomojiCode(message, ";waaa;", "＼(º □ º l|l)/")
+    message = replaceKaomojiCode(message, ";ohfuck;", "＼(〇_ｏ)／");
 
     //indifference
-    message = message
-        .split(";shrug;").join("¯\\_(ツ)_/¯")
-        .split(";iamout;").join("ᕕ( ᐛ )ᕗ");
+    message = replaceKaomojiCode(message, ";shrug;", "¯\\_(ツ)_/¯")
+    message = replaceKaomojiCode(message, ";iamout;", "ᕕ( ᐛ )ᕗ");
 
     //confused
-    message = message
-        .split(";wat;").join("(・・ ) ?");
+    message = replaceKaomojiCode(message, ";wat;", "(・・ ) ?");
 
     //doubt
-    message = message
-        .split(";doubt;").join("(⇀_⇀)");
+    message = replaceKaomojiCode(message, ";doubt;", "(⇀_⇀)");
 
     //suprised
-    message = message
-        .split(";wow;").join("w(°ｏ°)w")
-        .split(";oof;").join("(⊙_⊙)");
+    message = replaceKaomojiCode(message, ";wow;", "w(°ｏ°)w")
+    message = replaceKaomojiCode(message, ";oof;", "(⊙_⊙)");
 
     //greeting
-    message = message
-        .split(";hey;").join("＼(⌒▽⌒)")
-        .split(";oiii;").join("(≧▽≦)/");
+    message = replaceKaomojiCode(message, ";hey;", "＼(⌒▽⌒)")
+    message = replaceKaomojiCode(message, ";oiii;", "(≧▽≦)/");
 
     //hugging
-    message = message
-        .split(";happyhug;").join("(つ≧▽≦)つ")
-        .split(";sadhug;").join("(つ . •́ _ʖ •̀ .)つ");
+    message = replaceKaomojiCode(message, ";happyhug;", "(つ≧▽≦)つ")
+    message = replaceKaomojiCode(message, ";sadhug;", "(つ . •́ _ʖ •̀ .)つ");
 
     //winking
-    message = message
-        .split(";wink;").join("(^_<)〜☆");
+    message = replaceKaomojiCode(message, ";wink;", "(^_<)〜☆");
 
     //hiding
-    message = message
-        .split(";lurk;").join("|д･)");
+    message = replaceKaomojiCode(message, ";lurk;", "|д･)");
 
     //running
-    message = message
-        .split(";retreat;").join("ε=ε=┌( >_<)┘");
+    message = replaceKaomojiCode(message, ";retreat;", "ε=ε=┌( >_<)┘");
 
     //sleeping
-    message = message
-        .split(";tired;").join("(＿ ＿*) Z z z");
+    message = replaceKaomojiCode(message, ";tired;", "(＿ ＿*) Z z z");
 
     //cats
-    message = message
-        .split(";cat;").join("(=⌒‿‿⌒=)");
+    message = replaceKaomojiCode(message, ";cat;", "(=⌒‿‿⌒=)");
 
     //Escape backticks, since they are part of markdown
-    message = message
-        .split("`").join("\\`");
+    message = message.replace(/[`]/g, "\\`");
 
     return message;
+}
+
+function replaceKaomojiCode(message, code, kaomoji) {
+    codeEscaped = escapeRegExp(code)
+    return message.replace(new RegExp(codeEscaped, 'g'), kaomoji)
+}
+
+function escapeRegExp(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 }
