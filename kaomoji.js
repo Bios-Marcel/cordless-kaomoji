39cb7a7c8e18d9a39210c9b7e0ef222c69b30a10
Marcel Schramm <marceloschr@googlemail.com>
2019-02-05 20:48:11 +0100

    Remove useless semicolons and add super_table_flip

--- kaomoji.js
+++ kaomoji.js
@@ -3,74 +3,75 @@ function onMessageSend(message) {
     message = replaceKaomojiCode(message, ";uwu;", "(´･ω･`)")
     message = replaceKaomojiCode(message, ";yay;", "o(>ω<)o")
     message = replaceKaomojiCode(message, ";sparkle;", "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧")
-    message = replaceKaomojiCode(message, ";smile;", "(´･ᴗ･ ` )");
+    message = replaceKaomojiCode(message, ";smile;", "(´･ᴗ･ ` )")
 
     //dissatisfied
     message = replaceKaomojiCode(message, ";fucku;", "凸(￣ヘ￣)")
     message = replaceKaomojiCode(message, ";seriously;", "(；⌣̀_⌣́)")
-    message = replaceKaomojiCode(message, ";wtf;", "(」°ロ°)」");
+    message = replaceKaomojiCode(message, ";wtf;", "(」°ロ°)」")
 
     //anger
     message = replaceKaomojiCode(message, ";angry;", "(・`ω´・)")
     message = replaceKaomojiCode(message, ";ikillyou;", "↑_(ΦwΦ)Ψ")
-    message = replaceKaomojiCode(message, ";rage;", "٩(╬ʘ益ʘ╬)۶");
+    message = replaceKaomojiCode(message, ";rage;", "٩(╬ʘ益ʘ╬)۶")
+    message = replaceKaomojiCode(message, ";super_table_flip;", "┻━┻ ︵ヽ(`Д´)ﾉ︵﻿ ┻━┻")
 
     //empathy
-    message = replaceKaomojiCode(message, ";pat;", "(ｏ・_・)ノ”(ノ_<、)");
+    message = replaceKaomojiCode(message, ";pat;", "(ｏ・_・)ノ”(ノ_<、)")
 
     //embarassment
     message = replaceKaomojiCode(message, ";sweat;", "(⌒_⌒;)")
-    message = replaceKaomojiCode(message, ";embarassed;", "(⁄ ⁄•⁄ω⁄•⁄ ⁄)");
+    message = replaceKaomojiCode(message, ";embarassed;", "(⁄ ⁄•⁄ω⁄•⁄ ⁄)")
 
     //love
-    message = replaceKaomojiCode(message, ";love;", "(❤ω❤)");
+    message = replaceKaomojiCode(message, ";love;", "(❤ω❤)")
 
     //sadness
     message = replaceKaomojiCode(message, ";doh;", "(>_<)")
-    message = replaceKaomojiCode(message, ";cry;", ".｡･ﾟﾟ･(＞_＜)･ﾟﾟ･｡.");
+    message = replaceKaomojiCode(message, ";cry;", ".｡･ﾟﾟ･(＞_＜)･ﾟﾟ･｡.")
 
     //fear
     message = replaceKaomojiCode(message, ";waaa;", "＼(º □ º l|l)/")
-    message = replaceKaomojiCode(message, ";ohfuck;", "＼(〇_ｏ)／");
+    message = replaceKaomojiCode(message, ";ohfuck;", "＼(〇_ｏ)／")
 
     //indifference
     message = replaceKaomojiCode(message, ";shrug;", "¯\\_(ツ)_/¯")
-    message = replaceKaomojiCode(message, ";iamout;", "ᕕ( ᐛ )ᕗ");
+    message = replaceKaomojiCode(message, ";iamout;", "ᕕ( ᐛ )ᕗ")
 
     //confused
-    message = replaceKaomojiCode(message, ";wat;", "(・・ ) ?");
+    message = replaceKaomojiCode(message, ";wat;", "(・・ ) ?")
 
     //doubt
-    message = replaceKaomojiCode(message, ";doubt;", "(⇀_⇀)");
+    message = replaceKaomojiCode(message, ";doubt;", "(⇀_⇀)")
 
     //suprised
     message = replaceKaomojiCode(message, ";wow;", "w(°ｏ°)w")
-    message = replaceKaomojiCode(message, ";oof;", "(⊙_⊙)");
+    message = replaceKaomojiCode(message, ";oof;", "(⊙_⊙)")
 
     //greeting
     message = replaceKaomojiCode(message, ";hey;", "＼(⌒▽⌒)")
-    message = replaceKaomojiCode(message, ";oiii;", "(≧▽≦)/");
+    message = replaceKaomojiCode(message, ";oiii;", "(≧▽≦)/")
 
     //hugging
     message = replaceKaomojiCode(message, ";happyhug;", "(つ≧▽≦)つ")
     message = replaceKaomojiCode(message, ";sadhug;", "(つ . •́ _ʖ •̀ .)つ");
 
     //winking
-    message = replaceKaomojiCode(message, ";wink;", "(^_<)〜☆");
+    message = replaceKaomojiCode(message, ";wink;", "(^_<)〜☆")
 
     //hiding
-    message = replaceKaomojiCode(message, ";lurk;", "|д･)");
+    message = replaceKaomojiCode(message, ";lurk;", "|д･)")
 
     //running
-    message = replaceKaomojiCode(message, ";retreat;", "ε=ε=┌( >_<)┘");
+    message = replaceKaomojiCode(message, ";retreat;", "ε=ε=┌( >_<)┘")
 
     //sleeping
-    message = replaceKaomojiCode(message, ";tired;", "(＿ ＿*) Z z z");
+    message = replaceKaomojiCode(message, ";tired;", "(＿ ＿*) Z z z")
 
     //cats
-    message = replaceKaomojiCode(message, ";cat;", "(=⌒‿‿⌒=)");
+    message = replaceKaomojiCode(message, ";cat;", "(=⌒‿‿⌒=)")
 
-    return message;
+    return message
 }
 
 function replaceKaomojiCode(message, code, kaomoji) {
